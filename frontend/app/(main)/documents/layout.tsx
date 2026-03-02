3ee3d17ce7bc77ebf288279e5c3899dfdb81a9fd
ShubhPundir <shb.pndr@gmail.com>
2026-03-02 17:32:25 +0530

    del: removed frontend

--- frontend/app/(main)/documents/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function DocumentsLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children}</>;
-}
--- frontend/components/WorkflowGenerationLoader.tsx
+++
@@ -1,3 +0,0 @@
-// Re-export for backwards compatibility
-export { default } from './workflow-loader/Main';
-export * from './workflow-loader';
